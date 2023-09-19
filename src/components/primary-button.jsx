dbc5ad65366d6106dd9bb8fd740d9db0c8e5df57
Anemaygi <gianemayumisan@gmail.com>
2023-09-19 15:26:06 -0300

    feat: button hover animation

--- src/components/primary-button.jsx
+++ src/components/primary-button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function PrimaryButton(props){
 
   return(
-    <a className="bg-secondary text-primary px-5 py-2 rounded-full" href={props.href}>{props.name}</a>
+    <a className="bg-secondary text-primary px-5 py-2 rounded-full hover:shadow-number hover:-rotate-2 transition-all" href={props.href}>{props.name}</a>
   );
 
 }
--- src/components/secondary-button.jsx
+++ src/components/secondary-button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function SecondaryButton(props){
 
   return(
-    <a className="text-secondary px-5 py-2" href={props.href}> {props.name} </a>
+    <a className="text-secondary px-5 py-2 hover:scale-110 hover:-rotate-1 transition-all " href={props.href}> {props.name} </a>
   );
 
 }
