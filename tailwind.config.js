e09fb6c45599762a6b79eb3cc6645173e88d39a7
Anemaygi <gianemayumisan@gmail.com>
2023-09-19 15:27:12 -0300

    style: tailwind project extended theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,7 +6,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['Urbanist', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#119a5a',
+        secondary: '#c2fbd7',
+      },
+      dropShadow: {
+        'focus': 'rgba(44,187,99,.35) 0 -25px 18px -14px inset,rgba(44,187,99,.25) 0 1px 2px,rgba(44,187,99,.25) 0 2px 4px,rgba(44,187,99,.25) 0 4px 8px,rgba(44,187,99,.25) 0 8px 16px,rgba(44,187,99,.25) 0 16px 32px',
+        '3xl': '2px 2px 30px rgba(255, 255, 255, 1)',
+      },
+      boxShadow: {
+        'number': 'rgba(44, 187, 99, .2) 0 -25px 18px -14px inset,rgba(44, 187, 99, .15) 0 1px 2px,rgba(44, 187, 99, .15) 0 2px 4px,rgba(44, 187, 99, .15) 0 4px 8px,rgba(44, 187, 99, .15) 0 8px 16px,rgba(44, 187, 99, .15) 0 16px 32px',
+        'focus': 'rgba(44,187,99,.35) 0 -25px 18px -14px inset,rgba(44,187,99,.25) 0 1px 2px,rgba(44,187,99,.25) 0 2px 4px,rgba(44,187,99,.25) 0 4px 8px,rgba(44,187,99,.25) 0 8px 16px,rgba(44,187,99,.25) 0 16px 32px',
+      }
+    },
   },
   plugins: [],
 }
